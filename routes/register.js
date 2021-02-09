2d62dad23cdb593d73f5ff60baa6617a9167fb90
prnvthir14 <prnvthir@gmail.com>
2021-02-09 00:33:13 +0000

    add functionality to update our users database with new user registration info

--- routes/register.js
+++ routes/register.js
@@ -6,6 +6,12 @@ let router = express.Router();
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const { Pool } = require('pg');
+const dbParams = require('../lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
+
 
 router.route('/',(req,res) => {
 
@@ -14,14 +20,40 @@ router.route('/',(req,res) => {
 })
   .post((req,res) => {
 
-    res.send('logins post req');
+    console.log(req.body.Organization, 'ORGANIZATIONS')
+    let response = req.body;
+    const queryString = `
+    INSERT INTO users (name,email, password, admin,orgnization_id)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+    `;
+    const values = [response.fname, response.email,response.password, response.admin, response.Organization];
+     db.query(queryString,values)
+      .then(res => {
+        console.log(values)
+
+        return res.rows[0];
+      })
+      .catch(err => {
+        return console.log('query error:', err);
+      });
+
+
+
+
+
+    res.send('post register post req');
 
   }).get((req,res) => {
 
-    res.send("this is the registration page");
+    res.render('register');
 
   });
 
 
 
 module.exports = router;
+
+
+
+
