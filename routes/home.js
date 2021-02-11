13e86eae00c2a395a424270ee9d537739bb74c4d
prnvthir14 <prnvthir@gmail.com>
2021-02-11 23:49:13 +0000

    added functionality for select category from home page

--- routes/home.js
+++ routes/home.js
@@ -83,79 +83,32 @@ router.route('/:category')
   .get((req,res) => {
 
     const userEmail = req.session.user_email;
-    console.log(req.params);
-    const userEmailCookie = [`%${req.session.user_email}%`]; //seanPaul@eamil.com
+
+    const userEmailCookie = [`%${req.session.user_email}%`];
+
     const findUSerString = `SELECT id FROM users WHERE email LIKE $1;`;
-    //console.log(userEmailCookie);
+    const categoryForQuery = req.params.category;
+    //query to return user_login_per_site for given user and category
+    const returnFromQuery = (queryParams) => (db.query(`SELECT * FROM user_login_per_site WHERE user_id = $1 AND category = $2;`, queryParams));
+
     db.query(findUSerString, userEmailCookie)
       .then((dbres)=>{
 
-        const userID = [dbres.rows[0].id];
-        //console.log(userID);
+        const userID = dbres.rows[0].id;
 
-        //this will be the query to return grouped by result
-        return db.query(`SELECT * category FROM user_login_per_site WHERE user_id = $1 AND category = $2;`, userID);
+        const queryParams = [userID, categoryForQuery];
 
+        returnFromQuery(queryParams).then((data)=>{
 
-      }).then(dbres => {
-        const queryResults = dbres.rows;
+          console.log('DATA:', data);
 
-        console.log(queryResults);
+        }).catch(e => console.log('ERROR FROM RETURN categories',e));
 
-        let templateVars;
-        if (userEmail) {
-          templateVars =
-          { categories: queryResults,
-            idToStore: userEmail
-          };
-        } else {
-          templateVars =
-          { categories: queryResults,
-            idToStore: null
-          };
-        }
+      }).catch(e => console.log('ERROR RETURN FROM GET USER ID QUERY',e));
 
-        res.render('showCategories', templateVars);
-
-      }).then(dbres => {
-        //console.log(dbres); //works, retuns query results
-        //res.json(dbres.rows[0].password);
-        const queryResults = dbres.rows;
-        let templateVars;
-        if (userEmail) {
-          templateVars =
-          { passwords: queryResults,
-            idToStore: userEmail
-          };
-        } else {
-          templateVars =
-          { passwords: queryResults,
-            idToStore: null
-          };
-        }
-
-        res.render("myaccount",templateVars);
-      })
-      .catch(e => console.log('ERROR LINE 76',e));
+    res.send('error 126');
 
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////
-
-
 
 //get and post route to edit login
 router.route('/editLogin/:user_name_for_site_login')
